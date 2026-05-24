1e519b6325a068df084052c7f4deed55edfb806c
Developer <developer@example.com>
2026-05-24 16:32:10 +0200

    fix bundling bug

--- cdk/lib/website-stack.ts
+++ cdk/lib/website-stack.ts
@@ -117,8 +117,8 @@ export class WebsiteStack extends cdk.Stack {
         sources: [
           s3deploy.Source.asset(artifactsPath, {
             exclude: ['index.html'], 
-            // FORCE a unique hash by telling CDK to evaluate the hash *after* exclusions
-            assetHashType: cdk.AssetHashType.OUTPUT,
+            // Appending a different unique string ensures this generates a unique zip file inside cdk.out
+            assetHash: cdk.FileSystem.fingerprint(artifactsPath) + '-index-only',
           }),
         ],
         destinationBucket: websiteBucket,
@@ -135,8 +135,8 @@ export class WebsiteStack extends cdk.Stack {
         sources: [
           s3deploy.Source.asset(artifactsPath, {
             exclude: ['**/*', '!index.html'], // Exclude everything, but negate index.html
-            // FORCE a unique hash by telling CDK to evaluate the hash *after* exclusions
-            assetHashType: cdk.AssetHashType.OUTPUT,
+            // Appending a different unique string ensures this generates a unique zip file inside cdk.out
+            assetHash: cdk.FileSystem.fingerprint(artifactsPath) + '-index-only',
           }),
         ],
         destinationBucket: websiteBucket,
