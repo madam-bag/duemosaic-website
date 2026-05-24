8e7b2907a7d44d4584c991e1e1b416e97bbd67bf
Developer <developer@example.com>
2026-05-24 15:15:58 +0200

    fixed paths

--- cdk/lib/config.ts
+++ cdk/lib/config.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import * as path from 'path';
 
 // CodeStar Connection Configuration
 export const CODE_STAR_CONNECTION_ARN = 'arn:aws:codeconnections:us-east-1:210602314855:connection/2b41b7f8-4ce0-4248-8554-3e3f9ac605cd';
@@ -13,7 +14,7 @@ export const PIPELINE_NAME = 'WebsitePipeline';
 
 // Directory Configuration
 export const CDK_DIRECTORY = 'cdk';
-export const WEBSITE_ARTIFACTS_PATH = '../build'; // Relative to CDK directory
+export const WEBSITE_ARTIFACTS_PATH = path.resolve(__dirname, '..', '..', 'build');
 
 // DNS Configuration
 export const HOSTED_ZONE_NAME = 'duemosaic-architects.com';
